998dad81b36e8ca56b1a9aa97abbf64b16f231cb
mrohling13 <malachi.rohling@yahoo.com>
2025-01-30 22:32:55 -0500

    Task 1: Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,6 @@
+// Task 1: Product Price Management 
+let prices = [100, 200, 300, 400, 500]; // Array with 5 prices
+prices.push(600); // Adding my new price
+prices.shift(); // Removing the first price from the array
+console.log('Updated Price List:', prices); // Print Statement
+
